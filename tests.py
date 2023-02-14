268d72c42e020af9977f160301a1cb7ea33dca7f
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-14 19:24:53 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 # Initialize the Chrome browser
 browser = webdriver.Chrome()
