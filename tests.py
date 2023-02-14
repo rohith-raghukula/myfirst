f8cb591492131cdd492c02e2ff26d4615349ef6f
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-14 18:39:55 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,18 +1,30 @@
 from selenium import webdriver
 
-class TestGoogleSearch():
-    @classmethod
-    def setup_class(cls):
-        # Create a new Chrome browser instance
-        cls.driver = webdriver.Chrome()
-        cls.driver.implicitly_wait(10)
+# Create a new Chrome browser instance
+browser = webdriver.Chrome()
+cls.driver.implicitly_wait(10)
 
-    @classmethod
-    def teardown_class(cls):
-        # Close the browser
-        cls.driver.quit()
 
-    browser.get('https://profile.w3schools.com/')
+# Navigate to the website
+browser.get('https://example.com')
 
-      # Close the browser
+# Find the form fields and fill them in
+name_field = browser.find_element_by_name('name')
+name_field.send_keys('John Doe')
+email_field = browser.find_element_by_name('email')
+email_field.send_keys('johndoe@example.com')
+message_field = browser.find_element_by_name('message')
+message_field.send_keys('This is a test message')
+
+# Submit the form
+submit_button = browser.find_element_by_css_selector('button[type="submit"]')
+submit_button.click()
+
+# Wait for the response page to load
+browser.implicitly_wait(10)
+
+# Verify that the response page contains a success message
+assert 'Thank you for your message' in browser.page_source
+
+# Close the browser
 browser.quit()
