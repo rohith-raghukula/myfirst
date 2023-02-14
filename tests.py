dee9f9757a66e586062b1deea0205897ee864577
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-14 18:33:50 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,29 +1,22 @@
 from selenium import webdriver
+
 class TestGoogleSearch():
     @classmethod
     def setup_class(cls):
         # Create a new Chrome browser instance
         cls.driver = webdriver.Chrome()
         cls.driver.implicitly_wait(10)
-  
-# Navigate to the website
-browser.get('https://app.stezy.in/login')
 
-# Find the form fields and fill them in
-name_field = browser.find_element_by_name('username')
-name_field.send_keys('John Doe')
-email_field = browser.find_element_by_name('password')
-email_field.send_keys('johndoe@example.com')
+    @classmethod
+    def teardown_class(cls):
+        # Close the browser
+        cls.driver.quit()
 
+    def test_google_search(self):
+        # Navigate to the Google homepage
+        self.driver.get("https://www.google.com")
 
-# Submit the form
-submit_button = browser.find_element_by_css_selector('button[type="submit"]')
-submit_button.click()
-
-# Wait for the response page to load
-browser.implicitly_wait(10)
-
-
-
-# Close the browser
-browser.quit()
+        # Find the search input element and enter a search query
+        search_box = self.driver.find_element_by_name("q")
+        search_box.send_keys("python selenium")
+        search_box.submit()
