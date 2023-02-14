30d65225b7dde9171b02559f4f2aa1586a1d20ae
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-14 19:40:12 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class TestGoogleSearch():
     @classmethod
     def setup_class(cls):
@@ -14,12 +16,24 @@ class TestGoogleSearch():
 
     def test_google_search(self):
         # Navigate to the Google homepage
-        self.driver.get("https://www.google.com")
+        browser.get('https://profile.w3schools.com/')
 
-        # Find the search input element and enter a search query
-        search_box = self.driver.find_element_by_name("q")
-        search_box.send_keys("python selenium")
-        search_box.submit()
+# Find the email and password fields and enter the login credentials
+wait = WebDriverWait(browser, 10)
+email_field = wait.until(EC.presence_of_element_located((By.ID, 'modalusername')))
+email_field.send_keys('rohithr9701@gmail.com')
 
-        # Assert that the search results page was loaded successfully
-        assert "Python Selenium - Google Search" in self.driver.title
+password_field = wait.until(EC.presence_of_element_located((By.ID, 'current-password')))
+password_field.send_keys('Rohith@123')
+
+# Submit the login form
+password_field.submit()
+
+# Wait for the dashboard page to load
+wait.until(EC.url_contains('dashboard'))
+
+# Verify that the user is logged in and perform other actions on the dashboard page
+# ...
+
+# Close the browser
+browser.quit()
