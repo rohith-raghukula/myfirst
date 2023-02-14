7eb6c4e29d762ccb636c32eb552ee4590b770ce5
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-14 18:31:55 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,15 +7,14 @@ class TestGoogleSearch():
         cls.driver.implicitly_wait(10)
   
 # Navigate to the website
-browser.get('https://example.com')
+browser.get('https://app.stezy.in/login')
 
 # Find the form fields and fill them in
-name_field = browser.find_element_by_name('name')
+name_field = browser.find_element_by_name('username')
 name_field.send_keys('John Doe')
-email_field = browser.find_element_by_name('email')
+email_field = browser.find_element_by_name('password')
 email_field.send_keys('johndoe@example.com')
-message_field = browser.find_element_by_name('message')
-message_field.send_keys('This is a test message')
+
 
 # Submit the form
 submit_button = browser.find_element_by_css_selector('button[type="submit"]')
@@ -24,8 +23,7 @@ submit_button.click()
 # Wait for the response page to load
 browser.implicitly_wait(10)
 
-# Verify that the response page contains a success message
-assert 'Thank you for your message' in browser.page_source
+
 
 # Close the browser
 browser.quit()
