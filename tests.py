88b242b7c8f97149a79bab4747e1002b1f4ff2b2
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-15 10:54:47 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,27 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 
-# Set Chrome options to run in headless mode
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-
-# Create a Service object with the path to the Chrome driver executable
-service = Service('/path/to/chromedriver')
-
-# Initialize the Chrome browser with the headless options and the Service object
-browser = webdriver.Chrome(service=service, options=chrome_options)
-
+# Initialize the Chrome browser
+browser = webdriver.Chrome()
 
 # Navigate to the website login page
 browser.get('https://profile.w3schools.com/')
 
 # Find the email and password fields and enter the login credentials
 wait = WebDriverWait(browser, 10)
-email_field = wait.until(EC.presence_of_element_located((By.ID, 'modalusername')))
+email_field = wait.until(EC.presence_of_element_located((By.NAME, 'username')))
 email_field.send_keys('rohithr9701@gmail.com')
 
 password_field = wait.until(EC.presence_of_element_located((By.ID, 'current-password')))
