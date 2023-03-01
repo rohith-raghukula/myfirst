fe2f720205317c28c3512febc04ef4a8b9ef30b1
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-03-01 11:23:57 +0530

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -5,6 +5,12 @@ import time
 
 class TestStezyLogin(unittest.TestCase):
 
+        def setUpClass(cls):
+        # Configure Chrome options
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        
     @classmethod
     def setUpClass(cls):
         # Create a new Chrome browser instance
@@ -37,5 +43,4 @@ class TestStezyLogin(unittest.TestCase):
         # Check if login was successful
         self.assertIn("dashboard", self.driver.current_url, "Login failed")
         
-        if __name__ == '__main__':
-    unittest.main()
+  
