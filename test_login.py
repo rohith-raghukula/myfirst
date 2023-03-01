cb1362e233c75a4604cf37a958110ddc5a11e466
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-03-01 10:58:38 +0530

    Update and rename tests.py to test_login.py

---
+++ test_login.py
@@ -0,0 +1,44 @@
+import unittest
+import driver
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class TestStezyLogin(unittest.TestCase):
+
+
+    @classmethod
+    def setUpClass(cls):
+        # Create a new Chrome browser instance
+        cls.driver = webdriver.Chrome()
+
+        cls.driver.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Close the browser
+        cls.driver.quit()
+
+    def test_stezy_login(self):
+
+        # Navigate to the app.stezy.io homepage
+        driver = self.driver
+        driver.get("https://app.stezy.io")
+
+        # Find the email and password input fields and enter login credentials
+        email_input = driver.find_element(By.NAME, "username")
+        email_input.send_keys("rohit@stezy.io")
+
+        password_input = driver.find_element(By.NAME, "Password")
+        password_input.send_keys("Stezy@123")
+
+        # Find the login button and click it
+        login_button = driver.find_element(By.NAME, "LogIn")
+        login_button.click()
+
+
+        print("hi")
+        time.sleep(10)
+        # Check if login was successful
+        self.assertIn("dashboard", self.driver.current_url, "Login failed")
