cdc2f449a693c3e1c7b02944aa6a4a06efe14a35
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-03-01 11:17:07 +0530

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,18 +1,14 @@
 import unittest
-import driver
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
 class TestStezyLogin(unittest.TestCase):
 
-
     @classmethod
     def setUpClass(cls):
         # Create a new Chrome browser instance
         cls.driver = webdriver.Chrome()
-
         cls.driver.implicitly_wait(10)
 
     @classmethod
@@ -21,24 +17,22 @@ class TestStezyLogin(unittest.TestCase):
         cls.driver.quit()
 
     def test_stezy_login(self):
-
         # Navigate to the app.stezy.io homepage
-        driver = self.driver
-        driver.get("https://app.stezy.io")
+        self.driver.get("https://app.stezy.io")
 
         # Find the email and password input fields and enter login credentials
-        email_input = driver.find_element(By.NAME, "username")
+        email_input = self.driver.find_element(By.NAME, "username")
         email_input.send_keys("rohit@stezy.io")
 
-        password_input = driver.find_element(By.NAME, "Password")
+        password_input = self.driver.find_element(By.NAME, "Password")
         password_input.send_keys("Stezy@123")
 
         # Find the login button and click it
-        login_button = driver.find_element(By.NAME, "LogIn")
+        login_button = self.driver.find_element(By.NAME, "LogIn")
         login_button.click()
-
 
         print("hi")
         time.sleep(10)
+
         # Check if login was successful
         self.assertIn("dashboard", self.driver.current_url, "Login failed")
